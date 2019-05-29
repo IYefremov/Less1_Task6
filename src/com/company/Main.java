2341fa401ed596bf82a3e5c18f9fd2cff6267b94
IYefremov <iyefremov@cyberiansoft.com>
2019-05-29 17:04:40 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+/*Comment redundant strings to get the output "Happy Java Learning":
+public class Main {
+    public static void main(String[] args) {
+        String s = "Java";
+        System.out.println("Happy");
+        System.out.println("Java Learning");
+        System.out.println("programming");
+        System.out.print("Happy Java");
+        System.out.println("weekend");
+        System.out.println(s);
+        System.out.print("Python");
+        System.out.print(" ");
+        System.out.println("Learning");
+    }
+}
+*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // String s = "Java";
+        //System.out.println("Happy");
+        //System.out.println("Java Learning");
+        //System.out.println("programming");
+        System.out.print("Happy Java");
+        //System.out.println("weekend");
+        //System.out.println(s);
+        //System.out.print("Python");
+        System.out.print(" ");
+        System.out.println("Learning");
+
+    }
+}
